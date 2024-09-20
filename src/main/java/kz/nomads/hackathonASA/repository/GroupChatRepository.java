0718e5b1bbf7c9853440d7952f3bd160f549d768
coldkey12 <donvova110@mail.ru>
2024-09-20 14:54:09 +0500

    final

--- src/main/java/kz/nomads/hackathonASA/repository/GroupChatRepository.java
+++ src/main/java/kz/nomads/hackathonASA/repository/GroupChatRepository.java
@@ -2,6 +2,7 @@ package kz.nomads.hackathonASA.repository;
 
 import kz.nomads.hackathonASA.model.GroupChat;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.List;
 @Repository
 public interface GroupChatRepository extends JpaRepository<GroupChat, Long> {
     List<GroupChat> getGroupChatByOwnerId(Long id);
+
+    void deleteById(Long chatId);
 }
